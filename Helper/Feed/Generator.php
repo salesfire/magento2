8fe2252ccc31f8875414bc735d8972a675249fcd
Elliot Lings <elliot.lings@innolabs.uk>
2023-03-14 14:51:47 +0000

    Ensure strip_tags is passed a string as it can no longer accept null in PHP 8.

--- Helper/Feed/Generator.php
+++ Helper/Feed/Generator.php
@@ -223,7 +223,7 @@ class Generator
 
                         $text[] = ['<title><![CDATA[' . $this->escapeString($product->getName()) . ']]></title>', 3];
 
-                        $text[] = ['<description><![CDATA[' . $this->escapeString(substr($this->_escaper->escapeHtml(strip_tags($product->getDescription())), 0, 5000)) . ']]></description>', 3];
+                        $text[] = ['<description><![CDATA[' . $this->escapeString(substr($this->_escaper->escapeHtml(strip_tags($product->getDescription() ?: '')), 0, 5000)) . ']]></description>', 3];
 
                         $price = $this->getProductPrice($product);
                         $saleprice = $this->getProductSalePrice($product);
