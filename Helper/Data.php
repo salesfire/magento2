ad165c48a21635eff8347edbffbd369f468a9d18
James Horn <james.horn@salesfire.com>
2023-12-11 15:21:36 +0000

    [sc-8028] Fix php 7 compatibility issue.

--- Helper/Data.php
+++ Helper/Data.php
@@ -9,7 +9,7 @@ use Magento\Store\Model\ScopeInterface;
  *
  * @category   Salesfire
  * @package    Salesfire_Salesfire
- * @version    1.4.1
+ * @version    1.4.2
  */
 class Data extends AbstractHelper
 {
@@ -45,7 +45,7 @@ class Data extends AbstractHelper
      */
     public function getVersion()
     {
-        return '1.4.1';
+        return '1.4.2';
     }
 
     /**
